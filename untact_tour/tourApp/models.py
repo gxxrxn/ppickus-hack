a7a32584eeaef94e2bac3ebd27f38c255c93d2ad
gxxrxn <kkr1226@naver.com>
2020-09-24 18:39:45 +0900

    add: model 생성

--- untact_tour/tourApp/models.py
+++ untact_tour/tourApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model) :
+    user_name = models.CharField(max_length=20)
+    user_id = models.CharField(max_length=10)
+    user_pw = models.CharField(max_length=12)
+    user_email = models.CharField(max_length=20)
+
+    
